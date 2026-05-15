ceace3cfa4f1d462080cb7cfba800573f8964502
daemons <sihayas@icloud.com>
2026-05-15 17:02:10 -0400

    Initial Commit

---
+++ MaticDesignChallenge/MaticDesignChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaticDesignChallengeApp.swift
+//  MaticDesignChallenge
+//
+//  Created by daemons on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MaticDesignChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
